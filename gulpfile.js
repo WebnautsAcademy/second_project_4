2e8070d10ffec9f8b7deca3968807ce75cbc9009
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2021-07-12 17:20:17 +0300

    add files for blocks and variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 //Подключаем галп
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const less = require('gulp-less');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('html', () => {
   return gulp.src('source/*.html')
     .pipe(gulp.dest('./build'));
 });
 
+gulp.task("css", function () { 
+  return gulp.src("source/less/style.less") 
+    .pipe(less()) 
+    .pipe(gulp.dest("build/css")); 
+});
+gulp.task('imagemin', () => {
+  return gulp.src('source/img/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('build/img'))
+});
 
 gulp.task('watch', () => {
   browserSync.init({
@@ -17,7 +29,8 @@ gulp.task('watch', () => {
 
   gulp.watch('./source/*.html', gulp.series('html'))
   gulp.watch("./source/*.html").on('change', browserSync.reload);
+  gulp.watch('./source/img/*', gulp.series('imagemin'));
 });
 
 //Таск по умолчанию, Запускает del, styles, scripts и watch
-gulp.task('start', gulp.series('watch', 'html'));
+gulp.task('start', gulp.series('watch', 'html', 'imagemin'));
