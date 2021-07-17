920db435dd7117539fb3f6fea179fbac85c728a7
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2021-07-17 14:08:23 +0300

    problem with fonts

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,41 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const less = require('gulp-less');
 const imagemin = require('gulp-imagemin');
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const svgstore = require('gulp-svgstore');
 
 gulp.task('html', () => {
   return gulp.src('source/*.html')
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task("css", function () { 
-  return gulp.src("source/less/style.less") 
-    .pipe(less()) 
-    .pipe(gulp.dest("build/css")); 
+gulp.task("css", function () {
+  return (
+    gulp.src("source/less/style.less")
+      // обработчик ошибок
+      .pipe(plumber())
+      .pipe(less())
+      .pipe(gulp.dest("build/css"))
+  );
 });
 gulp.task('imagemin', () => {
   return gulp.src('source/img/*')
       .pipe(imagemin())
       .pipe(gulp.dest('build/img'))
 });
+
+gulp.task("sprite", function () { 
+  return gulp.src("source/img/*.svg") 
+    .pipe(imagemin([imagemin.svgo()])) 
+    .pipe(svgstore({ 
+      inlineSvg: true 
+    })) 
+    .pipe(rename("sprite.svg")) 
+    .pipe(gulp.dest("build/img")); 
+});
+
+
 
 gulp.task('watch', () => {
   browserSync.init({
@@ -27,10 +46,13 @@ gulp.task('watch', () => {
     }
   });
 
-  gulp.watch('./source/*.html', gulp.series('html'))
-  gulp.watch("./source/*.html").on('change', browserSync.reload);
+  gulp.watch('./source/*.html', gulp.series('html'));  
+  gulp.watch("./source/less/**/*.less", gulp.series("css"));
   gulp.watch('./source/img/*', gulp.series('imagemin'));
+  gulp.watch("./source/less/**/*.less").on("change", browserSync.reload);
+  gulp.watch("./source/*.html").on("change", browserSync.reload);
 });
 
 //Таск по умолчанию, Запускает del, styles, scripts и watch
-gulp.task('start', gulp.series('watch', 'html', 'imagemin'));
+
+gulp.task('start', gulp.series("imagemin", "html", "css", "watch" ));
