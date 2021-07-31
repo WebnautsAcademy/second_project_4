86f76c381faa1cabeacb8c00ae92e918f1d206df
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2021-07-31 23:45:56 +0300

    full page without adaptive
    Сверстал все блоки, осталось адаптив сделать

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,48 @@
 $(document).ready(function(){
     $('.content__item-title').click(function(){
         $(this).next('.content__item-text').slideToggle(200);
-        $(this).toggleClass('content__item-title--active '); 
-        
-      
+        $(this).toggleClass('content__item-title--active ');     
     });
+
+
+    // window.addEventListener('scroll', function(){
+    //   $('.footer__item').each(function(i, el){
+    //     const oldLeft = $(el).css('left').slice(0,-2)
+    //     console.log(i % 2 !== 0);
+    //     if (i % 2 !== 0) {
+    //       $(el).css('left', Number(oldLeft) - 20)
+    //     } else {
+    //       $(el).css('left', Number(oldLeft) + 20)
+    //     }
+    //   })
+    // })
 });
- 
+
+const swiper = new Swiper(".swiper-container", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  const swiperDecor = new Swiper(".swiper__decor", {
+    nested:true,
+     loop: true,
+    autoplay: {
+      delay: 1000,   
+    },
+    slidesPerView: 'auto',
+    // loopedSlides: 10,
+    // initialSlide: 0,
+  });
+
+    const swiperProject = new Swiper(".project-swiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  
